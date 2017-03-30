b04940337a93064d7142672cd1f31e09121ffecf
John Nash <jnash@workplacesystems.com>
2017-03-30 08:16:38 +0100

    Added failing encryption test.

--- src/test/java/name/jgn196/passwords/manager/AManager.java
+++ src/test/java/name/jgn196/passwords/manager/AManager.java
@@ -1,9 +1,11 @@
 package name.jgn196.passwords.manager;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -52,9 +54,19 @@ public class AManager {
     }
 
     @Test
-    public void storesFirstPassword() throws IOException {
+    public void createsPasswordStoreWhenPuttingFirstPassword() throws IOException {
 
         givenNoDataFile();
+        givenInput("bill_password", "file_password");
+
+        Manager.main(Command.PUT_COMMAND, "www.site.com", "Bill");
+
+        assertTrue(Files.exists(Paths.get(STORE_FILE_NAME)));
+    }
+
+    @Test
+    public void storesPassword() throws IOException {
+
         givenInput("bill_password", "file_password");
 
         Manager.main(Command.PUT_COMMAND, "www.site.com", "Bill");
@@ -62,7 +74,20 @@ public class AManager {
         assertThat(
                 capturedOutput(),
                 stringContainsInOrder(asList("Password for Bill @ www.site.com:", "Password for store:")));
-        assertTrue(Files.exists(Paths.get(STORE_FILE_NAME)));
+    }
+
+    @Test
+    @Ignore // TODO - Wire in encryption
+    public void encryptsPasswordInStoreFile() throws IOException {
+
+        final String password = "bill_password";
+        givenInput(password, "file_password");
+
+        Manager.main(Command.PUT_COMMAND, "www.site.com", "Bill");
+
+        assertFalse(
+                "Found password '" + password + "' in plain text in store file.",
+                fileContains(Manager.STORE_FILE_NAME, password));
     }
 
     @Test
@@ -90,5 +115,19 @@ public class AManager {
     private String capturedOutput() {
 
         return console.capturedOutput();
+    }
+
+    private boolean fileContains(final String fileName, final String pattern) throws IOException {
+
+        final byte[] fileData = Files.readAllBytes(Paths.get(fileName));
+
+        return contains(fileData, pattern.getBytes(StandardCharsets.UTF_8)) ||
+                contains(fileData, pattern.getBytes(StandardCharsets.UTF_16LE)) ||
+                contains(fileData, pattern.getBytes(StandardCharsets.UTF_16BE));
+    }
+
+    private boolean contains(byte[] data, byte[] pattern) {
+
+        return KMPMatch.indexOf(data, pattern) != -1;
     }
 }
