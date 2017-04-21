95e43b6bed8fb1a30d52c5cd42d1a0025a7797e4
John Nash <jnash@workplacesystems.com>
2017-04-21 11:51:11 +0100

    Added missing Remove command to general usage.

--- src/main/java/name/jgn196/passwords/manager/HelpCommand.java
+++ src/main/java/name/jgn196/passwords/manager/HelpCommand.java
@@ -5,7 +5,8 @@ class HelpCommand extends Command {
     static final String USAGE = "Commands:\n" +
             "\tlist\tLists all the stored logins.\n" +
             "\tget\tGet a password for a login.\n" +
-            "\tput\tPut a password for a login into the store.";
+            "\tput\tPut a password for a login into the store.\n" +
+            "\tremove\tRemove a password for a login.";
 
     @Override
     public void run(final Console console) {
