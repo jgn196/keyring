55a4a6b23d78bb6d7231223ca2a73d61ea94133b
John Nash <jnash@workplacesystems.com>
2017-05-03 09:10:21 +0100

    Added Salt unit tests.

---
+++ src/test/java/name/jgn196/passwords/manager/storage/ASalt.java
@@ -0,0 +1,63 @@
+package name.jgn196.passwords.manager.storage;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static name.jgn196.passwords.manager.storage.Salt.SALT_SIZE;
+import static org.junit.Assert.assertTrue;
+
+public class ASalt {
+
+    @Test
+    public void canBeGenerated() {
+
+        final Salt salt = new Salt();
+
+        assertTrue(isSalt(salt.toBytes()));
+    }
+
+    @Test
+    public void writesToStream() throws IOException {
+
+        try (final ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+
+            new Salt().writeTo(out);
+
+            assertTrue(isSalt(out.toByteArray()));
+        }
+    }
+
+    @Test
+    public void readsFromStream() throws IOException {
+        try (final ByteArrayInputStream in = new ByteArrayInputStream(new Salt().toBytes())) {
+
+            assertTrue(isSalt(Salt.readSaltFrom(in).toBytes()));
+        }
+    }
+
+    @Test(expected = IOException.class)
+    public void throwsIfSaltTruncated() throws IOException {
+        try (final ByteArrayInputStream in = new ByteArrayInputStream(new byte[SALT_SIZE / 2])) {
+
+            Salt.readSaltFrom(in);
+        }
+    }
+
+    private static boolean isSalt(final byte[] data) {
+
+        return data.length == SALT_SIZE && !isAllZero(data);
+    }
+
+    private static boolean isAllZero(final byte[] data) {
+
+        for (final byte d : data) {
+
+            if (d != 0) return false;
+        }
+
+        return true;
+    }
+}
