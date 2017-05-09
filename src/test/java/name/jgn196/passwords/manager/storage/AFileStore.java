773c0d6a69bde4e2ac185daa2fcd8d64ac4965f4
John Nash <jnash@workplacesystems.com>
2017-05-09 16:05:44 +0100

    Cleaned FileStore unit tests.

--- src/test/java/name/jgn196/passwords/manager/storage/AFileStore.java
+++ src/test/java/name/jgn196/passwords/manager/storage/AFileStore.java
@@ -6,11 +6,12 @@ import name.jgn196.passwords.manager.crypto.DecryptionFailed;
 import name.jgn196.passwords.manager.crypto.Salt;
 import name.jgn196.passwords.manager.crypto.SaltGenerator;
 import name.jgn196.passwords.manager.crypto.StoreEncryption;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
@@ -22,6 +23,12 @@ import static org.mockito.Mockito.*;
 public class AFileStore {
 
     private static final File TEST_FILE = new File("path");
+    private static final byte[] FILE_DATA = "file data".getBytes();
+    private static final byte[] CIPHER_TEXT = "cipher text".getBytes();
+    private static final Salt SALT = new SaltGenerator().get();
+    private static final byte[] PLAIN_TEXT = "plain text".getBytes();
+    private static final Crc32 CRC = Crc32.of(PLAIN_TEXT);
+    private static final StoreEntry STORE_ENTRY = new StoreEntry(new Login("www.site.com", "Bill"), Password.from("secret"));
 
     private final StoreFormat format = mock(StoreFormat.class);
     private final FileIO fileIO = mock(FileIO.class);
@@ -33,20 +40,29 @@ public class AFileStore {
             .with(fileIO)
             .build();
 
+    @Before
+    public void setUp() throws IOException {
+
+        when(fileIO.fileExists(TEST_FILE)).thenReturn(true);
+        when(fileIO.readAllFrom(TEST_FILE)).thenReturn(FILE_DATA);
+        when(format.deserialiseOuterLayer(FILE_DATA))
+                .thenReturn(new StoreFormat.DeserialiseResult(SALT, CRC, CIPHER_TEXT));
+        when(encryption.decryptWithSalt(CIPHER_TEXT, SALT)).thenReturn(PLAIN_TEXT);
+        when(format.deserialiseEntries(PLAIN_TEXT)).thenReturn(singletonList(STORE_ENTRY));
+    }
+
     @Test
     public void streamsNothingIfStoreFileMissing() {
 
         givenNoStoreFile();
 
-        assertEquals(0L, fileStore.stream().count());
+        final Stream<StoreEntry> results = fileStore.stream();
+
+        assertEquals(0L, results.count());
     }
 
     @Test
     public void readsFile() throws IOException {
-
-        givenStoreFileExists();
-        ignoreOuterLayerDeserialisation();
-        ignoreDecryption();
 
         fileStore.stream();
 
@@ -56,37 +72,23 @@ public class AFileStore {
     @Test
     public void deserialisesOuterFormatLayer() throws IOException {
 
-        final byte[] fileData = new byte[0];
-        givenStoreFileContains(fileData);
-        ignoreOuterLayerDeserialisation();
-        ignoreDecryption();
-
         fileStore.stream();
 
-        verify(format).deserialiseOuterLayer(fileData);
+        verify(format).deserialiseOuterLayer(FILE_DATA);
     }
 
     @Test
     public void decryptsCipherText() throws IOException {
 
-        final byte[] cipherText = new byte[0];
-        final Salt salt = new SaltGenerator().get();
-        givenStoreFileExists();
-        givenOuterFormatContains(cipherText, salt);
-        ignoreDecryption();
-
         fileStore.stream();
 
-        verify(encryption).decryptWithSalt(cipherText, salt);
+        verify(encryption).decryptWithSalt(CIPHER_TEXT, SALT);
     }
 
     @Test(expected = DecryptionFailed.class)
     public void failsIfPlainTextCrcIsWrong() throws IOException {
 
-        final byte[] pl = new byte[0];
-        givenStoreFileExists();
-        givenOuterFormatContains(notCrcOf(pl));
-        givenFileContentDecryptsTo(pl);
+        givenWrongCrc();
 
         fileStore.stream();
     }
@@ -94,23 +96,16 @@ public class AFileStore {
     @Test
     public void deserialisesEntries() throws IOException {
 
-        final byte[] pl = new byte[0];
-        final StoreEntry entry = new StoreEntry(new Login("www.site.com", "Bill"), Password.from("another secret"));
-        givenStoreFileExists();
-        ignoreOuterLayerDeserialisation();
-        givenFileContentDecryptsTo(pl);
-        givenPlDeserialisesTo(entry);
+        final Stream<StoreEntry> results = fileStore.stream();
 
-        assertThat(fileStore.stream().collect(toList()), contains(entry));
-
-        verify(format).deserialiseEntries(pl);
+        assertThat(results.collect(toList()), contains(STORE_ENTRY));
+        verify(format).deserialiseEntries(PLAIN_TEXT);
     }
 
     @Test(expected = StoreReadFailed.class)
     public void translatesIoErrorToStoreReadFailure() throws IOException {
 
-        when(fileIO.fileExists(TEST_FILE)).thenReturn(true);
-        when(fileIO.readAllFrom(TEST_FILE)).thenThrow(new IOException());
+        givenFileReadThrows();
 
         fileStore.stream();
     }
@@ -120,58 +115,14 @@ public class AFileStore {
         when(fileIO.fileExists(TEST_FILE)).thenReturn(false);
     }
 
-    private void givenStoreFileExists() throws IOException {
+    private void givenWrongCrc() throws IOException {
 
-        givenStoreFileContains(new byte[0]);
+        when(format.deserialiseOuterLayer(FILE_DATA))
+                .thenReturn(new StoreFormat.DeserialiseResult(SALT, new Crc32(CRC.toLong() + 1), CIPHER_TEXT));
     }
 
-    private void givenStoreFileContains(final byte[] value) throws IOException {
+    private void givenFileReadThrows() throws IOException {
 
-        when(fileIO.fileExists(TEST_FILE)).thenReturn(true);
-        when(fileIO.readAllFrom(TEST_FILE)).thenReturn(value);
-    }
-
-    private void ignoreOuterLayerDeserialisation() throws IOException {
-
-        givenOuterFormatContains(null, null);
-    }
-
-    private void givenOuterFormatContains(final byte[] cipherText, final Salt salt) throws IOException {
-
-        when(format.deserialiseOuterLayer(any(byte[].class)))
-                .thenReturn(new StoreFormat.DeserialiseResult(salt, new Crc32(0L), cipherText));
-    }
-
-    private void givenOuterFormatContains(final Crc32 crc) throws IOException {
-
-        when(format.deserialiseOuterLayer(any(byte[].class)))
-                .thenReturn(new StoreFormat.DeserialiseResult(null, crc, null));
-    }
-
-    private void ignoreDecryption() {
-
-        givenFileContentDecryptsTo(new byte[0]);
-    }
-
-    private void givenFileContentDecryptsTo(final byte[] pl) {
-
-        when(encryption.decryptWithSalt(any(byte[].class), any(Salt.class))).thenReturn(pl);
-    }
-
-    private Crc32 notCrcOf(final byte[] data) {
-
-        return new Crc32(Crc32.of(data).toLong() + 1L);
-    }
-
-    private void givenPlDeserialisesTo(final StoreEntry entry) throws IOException {
-
-        when(format.deserialiseEntries(any(byte[].class))).thenReturn(singletonList(entry));
-    }
-
-    private byte[] writtenData() throws IOException {
-
-        final ArgumentCaptor<byte[]> dataCaptor = ArgumentCaptor.forClass(byte[].class);
-        verify(fileIO).writeAllTo(eq(TEST_FILE), dataCaptor.capture());
-        return dataCaptor.getValue();
+        when(fileIO.readAllFrom(TEST_FILE)).thenThrow(new IOException());
     }
 }
