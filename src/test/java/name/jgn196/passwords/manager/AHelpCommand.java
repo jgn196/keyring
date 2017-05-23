4bc03c158e0e7838cad41b76c951ca90addc7ba0
John Nash <jnash@workplacesystems.com>
2017-05-23 09:45:23 +0100

    Moved to semantic versioning.

--- src/test/java/name/jgn196/passwords/manager/AHelpCommand.java
+++ src/test/java/name/jgn196/passwords/manager/AHelpCommand.java
@@ -1,8 +1,11 @@
 package name.jgn196.passwords.manager;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class AHelpCommand {
 
@@ -14,5 +17,16 @@ public class AHelpCommand {
         new HelpCommand(console).run();
 
         assertEquals(HelpCommand.USAGE, console.capturedOutput());
+    }
+
+    @Test
+    @Ignore
+    public void printsApplicationVersion() {
+
+        final TestConsole console = new TestConsole();
+
+        new HelpCommand(console).run();
+
+        assertThat(console.capturedOutput(), containsString("1.0.0"));
     }
 }
